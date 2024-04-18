2a0248da4f1ab4c12fe47cdcfe04f36e3b1edf81
vitorbetmann <vitorbetmann@me.com>
2024-04-18 16:01:30 -0400

    Fix small bug related to selecting 'featured' option on dropdown-menu

--- client.js
+++ client.js
@@ -18,8 +18,8 @@ function display(className) {
 
 function show(value) {
   var obj = document.getElementsByClassName("projects")[0].children;
-  for (var i = 0; i < obj.length; i++) {
-    if (obj[i].className == value) {
+  for (var i = 1; i < obj.length; i++) {
+    if (obj[i].className == value || value == "featured") {
       obj[i].style.display = "contents";
     } else {
       obj[i].style.display = "none";
